263c52a69d7c0c648c499bdcf144e182759ee358
Alex Gavrilescu <leskcorp@gmail.com>
2026-01-18 17:57:52 +0100

    Update packages

--- styles/index.ts
+++ styles/index.ts
@@ -1,5 +1,5 @@
 // inherit from base layouts, remove it to get full customizations
 import '@slidev/client/styles/layouts-base.css'
-import '@alvarosabu/ui/styles'
+import '@alvarosabu/ui/dist/as-ui.css'
 import './fonts.css'
 import './layout.scss'
